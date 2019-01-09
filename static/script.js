b98d7506a7fa7c8e9955061c665d8984c503d267
plOnion <damian.lukasiewicz@gmail.com>
2019-01-09 20:34:05 +0100

    BEM methodology changes / template string fix / parseInt second parametr added / hide class added / searching and sorting functions fixed

--- static/script.js
+++ static/script.js
@@ -1,117 +1,114 @@
-$(function() {
+$(function () {
 
-    let main = $(".js-content");
-    const url = "http://localhost:3000";
+    const main = $(".js-content");
+    const url = "http://localhost:3000/channels.json";
+    const channelsList = document.querySelector('.js-content');
+    const channelSearch = document.querySelector('.filter__input');
+    const radios = document.querySelectorAll('input[type=radio]');
+    const button = document.querySelector(".button");
 
-        $.ajax({
-            method: "GET",
-            url: url + "/channels.json",
-            dataType: "json"
-        }).done(response => {
-            console.log(response);
+    $.ajax({
+        method: "GET",
+        url: url,
+        dataType: "json"
+    }).done(response => {
 
-            const channels = Object.assign([], response);
-            const channelsList = document.querySelector('.js-content');
-            const channelSearch = document.querySelector('.filter__input');
-            let radios = document.querySelectorAll('input[type=radio]');
+        const channels = Object.assign([], response);
 
+        showChannel(channels);
 
-            const showChannel = (elem) => {
-                elem.forEach(el => {
+        function changeHandler() {
+            if (this.value === 'titleSort') {
+                titleSort(channels);
 
-                    main.append(`<div class='channel'>` +
-                        `<div><a href=`+ el.customUrl + ` target='_blank'><img class='channel_logo' src=` + el.thumbnails.default.url + ` /></a></div>` +
-                        `<h2 class='bold'>` + el.title + `</h2>` +
-                        `<div class='channel_stats'>` +
-                        `<div class='center'><span class='channel_stats_description'>SUBSCRIBERS:</span></br> <span class='bold'>` + parseInt(el.statistics.subscriberCount).toLocaleString('en-US') + `</span></div>`
-                        +
-                        `<div class='center'><span class='channel_stats_description'>VIDEOS:</span></br><span class='bold'>` + parseInt(el.statistics.videoCount).toLocaleString('en-US') + `</span></div>` +
-                        `<div class='center'><span class='channel_stats_description'>VIEWS:</span></br><span class='bold'>` + parseInt(el.statistics.viewCount).toLocaleString('en-US') + `</span></div>` +
-                        `</div>` +
-                        `</div>`);
+            } else if (this.value === 'subsSort') {
+                subsSort(channels);
 
-                });
-            };
+            } else if (this.value === 'videoSort') {
+                videoSort(channels);
 
-            showChannel(channels);
-
-            const titleSort = () => {
-                (channels.sort(function (a, b) {
-                    return a.title.localeCompare(b.title);
-                }));
-                clearList();
-                showChannel(channels);
-            };
-
-            const subsSort = () => {
-                (channels.sort((a, b) => parseFloat(b.statistics.subscriberCount) - parseFloat(a.statistics.subscriberCount)));
-                clearList();
-                showChannel(channels);
-            };
-
-            const videoSort = () => {
-                (channels.sort((a, b) => parseFloat(b.statistics.videoCount) - parseFloat(a.statistics.videoCount)));
-                clearList();
-                showChannel(channels);
-            };
-
-            const viewSort = () => {
-                (channels.sort((a, b) => parseFloat(b.statistics.viewCount) - parseFloat(a.statistics.viewCount)));
-                clearList();
-                showChannel(channels);
-            };
-
-
-
-            function changeHandler(event) {
-                if ( this.value === 'titleSort' ) {
-                    titleSort();
-
-                } else if ( this.value === 'subsSort' ) {
-                    subsSort();
-
-                } else if ( this.value === 'videoSort' ) {
-                    videoSort();
-
-                } else if ( this.value === 'viewSort' ) {
-                    viewSort();
-                }
+            } else if (this.value === 'viewSort') {
+                viewSort(channels);
             }
+        }
 
-            Array.prototype.forEach.call(radios, function(radio) {
-                radio.addEventListener('change', changeHandler);
-            });
-
-
-            channelSearch.addEventListener('input', function() {
-                const val = this.value;
-                const channel = channelsList.querySelectorAll('.channel');
-
-                [].forEach.call(channel, function(el) {
-                    const text = el.querySelector('h2').innerText;
-
-                    if (text.toLowerCase().indexOf(val.toLowerCase()) !== -1) {
-                        el.style.setProperty('display', '');
-                    } else {
-                        el.style.setProperty('display', 'none');
-                    }
-                });
-            });
-
-
-            const clearList = () => { channelsList.innerHTML = "" };
-
-
-            $(".button").on("click", function () {
-                $(".choice--radio").prop("checked", false);
-                $(".filter__input").val("");
-
-                clearList();
-                showChannel(response);
-            })
-
-
-
+        [].forEach.call(radios, radio => {
+            radio.addEventListener('change', changeHandler);
         });
 
+        channelSearch.addEventListener('input', function () {
+            search();
+        });
+
+
+        button.addEventListener("click", () => {
+            $(".choice--radio").prop("checked", false);
+            channelSearch.value = "";
+
+            clearList();
+            showChannel(response);
+        })
     });
+
+    const showChannel = (elem) => {
+        elem.forEach(el => {
+
+            const subs = parseInt(el.statistics.subscriberCount, 10).toLocaleString('en-US');
+            const videos = parseInt(el.statistics.videoCount, 10).toLocaleString('en-US');
+            const views = parseInt(el.statistics.viewCount, 10).toLocaleString('en-US');
+
+            main.append(`<div class='main__channel'>
+                    <div><a href= ${el.customUrl} target='_blank'><img class='channel__logo' src= ${el.thumbnails.default.url} /></a></div>
+                    <h2 class='bold'> ${el.title} </h2>
+                    <div class='channel__stats'>
+                        <div><span class='stats__description'>SUBSCRIBERS:</span></br> <span class='bold'>${subs}</span></div>
+                        <div><span class='stats__description'>VIDEOS:</span></br><span class='bold'>${videos}</span></div>
+                        <div><span class='stats__description'>VIEWS:</span></br><span class='bold'>${views}</span></div>
+                    </div>
+                    </div>`);
+        });
+    };
+
+    const search = () => {
+        if (channelSearch.value !== ""){
+        const val = channelSearch.value;
+        const channel = channelsList.querySelectorAll('.main__channel');
+
+        [].forEach.call(channel, (el) => {
+            const text = el.querySelector('h2').innerText;
+
+            text.toLowerCase().indexOf(val.toLowerCase()) !== -1 ? el.classList.remove("hide") : el.classList.add("hide")
+        });
+    }};
+
+    const sort = (channels) => {
+        clearList(channelsList);
+        showChannel(channels);
+        search();
+    }
+
+    const clearList = () => {
+        channelsList.innerHTML = ""
+    };
+
+    const titleSort = (channels) => {
+        (channels.sort((a, b) => a.title.localeCompare(b.title)));
+        sort(channels);
+
+    };
+
+    const subsSort = (channels) => {
+        (channels.sort((a, b) => parseInt(b.statistics.subscriberCount) - parseInt(a.statistics.subscriberCount)));
+        sort(channels);
+    };
+
+    const videoSort = (channels) => {
+        (channels.sort((a, b) => parseInt(b.statistics.videoCount) - parseInt(a.statistics.videoCount)));
+        sort(channels);
+    };
+
+    const viewSort = (channels) => {
+        (channels.sort((a, b) => parseInt(b.statistics.viewCount) - parseInt(a.statistics.viewCount)));
+        sort(channels);
+    };
+});
